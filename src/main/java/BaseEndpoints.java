35b073dbeae650ff83924afe31dc9769e10e6fc9
Tatyana Sharova <lequimera@yandex.ru>
2022-04-10 19:53:03 +0300

    First commit

---
+++ src/main/java/BaseEndpoints.java
@@ -0,0 +1,8 @@
+public final class BaseEndpoints {
+    public final static String COURIER_LOGIN = "/api/v1/courier/login";
+    public final static String COURIER_REGISTER_OR_DELETE = "/api/v1/courier/";
+    public final static String ORDER_CREATE_OR_GET = "/api/v1/orders";
+    public final static String ORDER_GET_BY_TRACK = "api/v1/orders/track";
+    public final static String ORDER_CANCEL = "api/v1/orders/cancel";
+    public final static String ORDER_ACCEPT = "api/v1/orders/accept/";
+}
